132c063148f711cbcca2e293932a411820232aae
池田慎哉 <ikeshin0509@gmail.com>
2020-01-20 23:40:29 +0900

    得点の表示と左右フリッパーのタッチ対応
    対象物に衝突した際の合計得点とテキストの表示、並びにフリッパーのタッチとの連動

---
+++ Assets/Materials/TotalScore.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TotalScore : MonoBehaviour {
+
+	public int CloudPoint = 10; //雲の点数
+	public int LargeStarPoint = 15; //大きな星の点数
+	public int SmallStarPoint = 20; //小さな雲の点数
+	public int TS; 
+	GameObject AS; 
+
+	// Use this for initialization
+	void Start () {
+
+			//シーン中からオブジェクトを取得
+ 		this.AS = GameObject.Find("TotalScore"); 
+	}
+
+	// Update is called once per frame
+	public void Update (){
+
+	//トータルスコアを取得
+
+		AS.GetComponent<Text> ().text = "Total Score" + TS;
+
+	}
+		 
+	public void OnCollisionEnter (Collision other) { 
+			
+			if(other.gameObject.tag == "SmallStarTag"){
+				//オブジェクトに応じてスコアを加算　
+			TS += SmallStarPoint;
+			}else if(other.gameObject.tag == "LargeStarTag"){
+				//オブジェクトに応じてスコアを加算　
+			TS += CloudPoint;
+			}else if(other.gameObject.tag == "LargeCloudTag" || other.gameObject.tag == "SmallCloudTag"){
+				//オブジェクトによってスコアを加算
+			TS += LargeStarPoint;
+
+		}
+			}
+
+}
+
---
+++ Assets/Materials/TouchMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchMovement : MonoBehaviour
+{
+
+	//HingeJointコンポーネントを入れる　
+	private HingeJoint myHingeJoint; 
+
+	//初期の傾き　
+	private float defaultAngle = 20; 
+	//弾いた時の傾き　
+	private float flickAngle = -20; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		//HIngeJointコンポーネント取得
+		this.myHingeJoint = GetComponent<HingeJoint> (); 
+
+		//フリッパーの傾きを設定　
+		SetAngle(this.defaultAngle); 
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		//タッチがされている場合
+		if (0 < Input.touchCount) 
+		{
+			//タッチのカウンター関数を起動
+			for (int i = 0; i < Input.touchCount; i++) 
+			{
+				//（i）番目のタッチを取得
+				Touch t = Input.GetTouch (i);
+
+				//タッチしたかどうか
+				if (t.phase == TouchPhase.Began) 
+				{ 
+					//画面上のどこをタッチしたかによって左右フリッパーの動きを変える
+					if (t.position.x >= 375 && tag == "RightFripperTag") {
+						SetAngle (this.flickAngle);
+						Debug.Log (t.position.x);
+					} else if (t.position.x <= 375 && tag == "LeftFripperTag") {
+						SetAngle (this.flickAngle);
+					}
+					Debug.Log (t.position.x);
+				}
+			
+		
+					
+				//タッチされなくなった場合
+					if (t.phase == TouchPhase.Ended) 
+				{ 
+						if (tag == "RightFripperTag") 
+					{    
+							SetAngle (this.defaultAngle);
+							Debug.Log ("動いている");
+					}
+				else if (tag == "LeftFripperTag") 
+					{	
+					
+						SetAngle (this.defaultAngle);
+						Debug.Log ("動いている");
+					} 
+
+				}
+			}
+		}	
+	}
+
+		//フリッパーの傾きを設定
+     public void SetAngle (float angle)
+	{
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = angle; 
+					this.myHingeJoint.spring = jointSpr;
+	}
+}			
+
+
